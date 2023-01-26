6e95ce240ed2c97ba83f9b6f78144f4e069c58d0
Tim Wu <61076879+theredwuster@users.noreply.github.com>
2023-01-26 10:02:06 -0800

    Update HomePageView.swift

--- PEMPal.1.0/HomePageView.swift
+++ PEMPal.1.0/HomePageView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct HomePageView: View {
     var body: some View {
         ScrollView{
-ian-hall1-patch-2
             HStack{
                 Spacer()
                 Image("logo")
