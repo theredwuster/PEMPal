a8680a305b553ac5f93cce12b661d11e9aa31d0a
Tim Wu <twuster@stanford.edu>
2023-03-15 22:28:46 -0700

    Updated ReadMe and File Naming

---
+++ PEMPal.1.0/PEMPal_App.swift
@@ -0,0 +1,21 @@
+//
+//  PEMPal_App.swift
+//  PEMPal.1.0
+//
+//  Created by Tim Wu on 1/11/23.
+//
+
+import SwiftUI
+import HealthKit
+
+@main
+struct PEMPal_1_0App: App {
+    
+    @ObservedObject var globalModel = GlobalModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            InitializeView(globalModel: globalModel)
+        }
+    }
+}
